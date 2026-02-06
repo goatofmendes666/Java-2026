04a030884d18df74b7689c363b8f92c70736d989
USER <garvagarwal97@gmail.com>
2026-02-06 17:06:18 +0530

    Basic Hello World

---
+++ Basic Java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World!"); // A simple Hello World Program.
+        System.out.println("You can also print the sentences without breaking lines.");
+        System.out.print("Like this, ");
+        System.out.print("and this");
+    }
+}
