1d86d64af28ebd17dc842b27c656112015f3c11e
USER <garvagarwal97@gmail.com>
2026-02-07 09:27:10 +0530

    Practicing Operators

---
+++ Basic Java/ArithmaticOp.java
@@ -0,0 +1,20 @@
+public class ArithmaticOp
+{
+    public static void main(String[] args)
+    {
+        int a = 20;
+        int b = 4;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        int z = 5;
+        ++z;
+        System.out.println(z);
+        --z;
+        System.out.println(z);
+    }
+}
