a9abf42b8db2688ac3a3a17a328a0d681572b866
USER <garvagarwal97@gmail.com>
2026-02-07 23:13:56 +0530

    Compares multiple values to determine minimum, maximum, or equality.

---
+++ Basic Java/Valuecomparer.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Valuecomparer
+{
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a == b)
+        {
+            System.out.println("equal");
+        }
+        else if (a > b)
+        {
+            System.out.println("Greater");
+        }
+        else
+        {
+            System.out.println("Lesser");
+        }
+    }
+}
