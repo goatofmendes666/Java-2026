fdf50fcdbe3c3a3f69cdf451b6233d8ab65e84e1
USER <garvagarwal97@gmail.com>
2026-02-07 10:16:32 +0530

    Practicing variables and Arithmetics.

---
+++ Basic Java/printNumbers.java
@@ -0,0 +1,13 @@
+public class printNumbers
+{
+    public static void main(String[] args)
+    {
+        // we can also print numbers and do all type of stuffs like arithmetics also.
+        System.out.println(25);
+        System.out.println(1+1);
+        System.out.println(3-1);
+        System.out.println(5*2);
+        System.out.println(18/3);
+        System.out.println(7%2);
+    }
+}
