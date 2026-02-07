ea07e46814ecabc971b2c64338f4897db3155d72
USER <garvagarwal97@gmail.com>
2026-02-07 09:24:48 +0530

    Practicing Type casting

---
+++ Basic Java/Casting2.java
@@ -0,0 +1,14 @@
+public class Casting2
+{
+    public static void main(String[] args)
+    {
+      int rahul = 87;
+      double tanya = 100.0d;
+
+      double Average = rahul/5;
+      int average = (int) tanya/5;
+
+      System.out.println(Average);
+      System.out.println(average);
+    }
+}
