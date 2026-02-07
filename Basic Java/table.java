442604fdb677b1b96932e801b95f9c3a536fc280
USER <garvagarwal97@gmail.com>
2026-02-07 23:13:00 +0530

    Simple multiplication table generator.

---
+++ Basic Java/table.java
@@ -0,0 +1,11 @@
+public class table
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 2; j <= 2; j++) {
+                System.out.println(i * j);
+            }
+        }
+    }
+}
