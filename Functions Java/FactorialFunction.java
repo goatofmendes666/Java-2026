41198c7560b8e98856b9a30b1e39fed0f8092bab
USER <garvagarwal97@gmail.com>
2026-02-08 12:34:39 +0530

    Implements a standalone function to calculate the factorial of a number.

--- Functions Java/FactorialFunction.java
+++ Functions Java/FactorialFunction.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class FactorialFunction {
+    public static void calculateFactorial(int n){
+        int factorial = 1;
+        if (n < 0)
+        {
+            System.out.println("Factorial is not defined for negative numbers");
+            return;
+        } else if (n == 1 || n == 0) {
+            System.out.println("Factorial of " + n + " is 1");
+            return;
+        }
+
+        for (int i = n; i >= 1; i--)
+        {
+            factorial = factorial * i;
+        }
+        System.out.println("Factorial of " + n + " is " + factorial);
+        return;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number to print the factorial : ");
+        int n = sc.nextInt();
+
+        calculateFactorial(n);
+    }
 }
